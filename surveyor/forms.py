5e97ab86db8bf49e3d9f3e2eb5354cc5d253d342
Spencer Judd <spencercjudd@gmail.com>
2013-07-02 10:38:31 -0400

    Refactored DynamicSurveyFormMeta again.
    Traded a little simplicity for more robustness. If a field type's
    declaration can't be found in the global namespace, we will now attempt
    to import it from wtforms.

--- surveyor/forms.py
+++ surveyor/forms.py
@@ -1,5 +1,4 @@
 from flask.ext.wtf import Form
-from wtforms import TextField
 from wtforms.form import FormMeta
 
 class DynamicSurveyFormMeta(FormMeta):
@@ -7,8 +6,16 @@ class DynamicSurveyFormMeta(FormMeta):
         if survey:
             cls.survey_name = survey["name"]
             for f in survey["fields"]:
-                field = globals()[f["type"]](f["name"])
-                setattr(cls, f["slug"], field)
+                # Search for an object matching _type in the global namespace.
+                # Failing that, try importing said object from wtforms.
+                # If found, set the object as a class attribute.
+                _name, _slug, _type = f["name"], f["slug"], str(f["type"])
+                field = globals().get(_type)
+                if not field:
+                    _tmp = __import__("wtforms", globals(), locals(), [_type], -1)
+                    field = getattr(_tmp, _type)
+                field = field(_name)
+                setattr(cls, _slug, field)
         return super(DynamicSurveyFormMeta, cls).__call__(*args, **kwargs)
 
 class DynamicSurveyForm(Form):
